172cf26d33ca52b5400032e8a8795f06dfe977c6
howetuft <howetuft@gmail.com>
2024-07-16 23:22:18 +0200

    Try to cache

--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
-export CIBW_DEBUG_KEEP_CONTAINER=TRUE
-act --action-offline-mode --matrix os:ubuntu-latest --matrix python-minor:'7'
+#export CIBW_DEBUG_KEEP_CONTAINER=TRUE
+act --action-offline-mode -s GITHUB_TOKEN="$(gh auth token)" --matrix os:ubuntu-latest --matrix python-minor:'7'
--- scripts/before_all_linux.sh
+++ scripts/before_all_linux.sh
@@ -2,6 +2,18 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+echo "BEFORE ALL LINUX"
+
+echo $ACTIONS_CACHE_URL
+echo $ACTIONS_RUNTIME_TOKEN
+
+# sccache
+tar -xvzf /project/externals/linux/sccache-v0.8.1-x86_64-unknown-linux-musl.tar.gz -C .
+mv sccache-v0.8.1-x86_64-unknown-linux-musl/sccache /usr/bin
+export SCCACHE_DIRECT=1
+export SCCACHE_GHA_ENABLED=1
+sccache --show-stats
+
 # Embree
 EMBREE_VERSION=3.8.0  # Last version with rpm (Apr 7, 2020)
 tar -xvzf /project/externals/linux/embree-${EMBREE_VERSION}.x86_64.rpm.tar.gz -C .
@@ -16,8 +28,8 @@ cp ${OIDN_DIR}/bin/* /usr/bin
 mkdir /usr/share/doc/OpenImageDenoise
 cp ${OIDN_DIR}/doc/* /usr/share/doc/OpenImageDenoise
 cp -r ${OIDN_DIR}/include/OpenImageDenoise /usr/include
-
 cp -r ${OIDN_DIR}/lib/* /usr/lib
+
 dnf -y install epel-release && /usr/bin/crb enable
 dnf -y install libtiff-devel libjpeg-devel libpng-devel gtk3-devel OpenImageIO-devel OpenEXR-devel boost-devel boost-python3-devel boost-numpy3 flex tbb-devel blosc-devel
 
