94617553fabf0a01d11c020ca1180040d6bfc538
howetuft <howetuft@gmail.com>
2024-07-19 06:43:13 +0200

    Lint debug.sh

--- debug.sh
+++ debug.sh
@@ -1,2 +1,13 @@
+# SPDX-FileCopyrightText: 2024 Howetuft
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Script to run build locally. You need 'act' to be installed in your
+# system
+
 #export CIBW_DEBUG_KEEP_CONTAINER=TRUE
-act --action-offline-mode -s GITHUB_TOKEN="$(gh auth token)" --matrix os:ubuntu-latest --matrix python-minor:'7'
+act \
+  --action-offline-mode \
+  -s GITHUB_TOKEN="$(gh auth token)" \
+  --matrix os:ubuntu-latest --matrix python-minor:'7' \
+  --artifact-server-path /tmp/artifacts
