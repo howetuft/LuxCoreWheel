e3bd7c7a4cb03d0480b0ed8cb8de5e4bee4752c9
howetuft <howetuft@gmail.com>
2024-07-19 06:45:35 +0200

    Before_all_linux: silence untar

--- scripts/before_all_linux.sh
+++ scripts/before_all_linux.sh
@@ -5,25 +5,29 @@
 echo "BEFORE ALL LINUX"
 
 # sccache
-tar -xvzf /project/externals/linux/sccache-v0.8.1-x86_64-unknown-linux-musl.tar.gz -C .
+echo "Installing sccache"
+tar -xzf /project/externals/linux/sccache-v0.8.1-x86_64-unknown-linux-musl.tar.gz -C .
 mv sccache-v0.8.1-x86_64-unknown-linux-musl/sccache /usr/bin
 
 # Embree
+echo "Installing embree"
 EMBREE_VERSION=3.8.0  # Last version with rpm (Apr 7, 2020)
-tar -xvzf /project/externals/linux/embree-${EMBREE_VERSION}.x86_64.rpm.tar.gz -C .
+tar -xzf /project/externals/linux/embree-${EMBREE_VERSION}.x86_64.rpm.tar.gz -C .
 dnf -y localinstall embree3-devel-${EMBREE_VERSION}-1.noarch.rpm
 dnf -y localinstall embree3-lib-${EMBREE_VERSION}-1.x86_64.rpm
 
 # OIDN
+echo "Installing OIDN"
 OIDN_VERSION=1.2.4  # Last release with libtbb.so.2
 OIDN_DIR=oidn-${OIDN_VERSION}.x86_64.linux
-tar -xvzf /project/externals/linux/oidn-${OIDN_VERSION}.x86_64.linux.tar.gz -C .
+tar -xzf /project/externals/linux/oidn-${OIDN_VERSION}.x86_64.linux.tar.gz -C .
 cp ${OIDN_DIR}/bin/* /usr/bin
 mkdir /usr/share/doc/OpenImageDenoise
 cp ${OIDN_DIR}/doc/* /usr/share/doc/OpenImageDenoise
 cp -r ${OIDN_DIR}/include/OpenImageDenoise /usr/include
 cp -r ${OIDN_DIR}/lib/* /usr/lib
 
+# Distro Packages
 dnf -y install epel-release && /usr/bin/crb enable
 dnf -y install libtiff-devel libjpeg-devel libpng-devel gtk3-devel OpenImageIO-devel OpenEXR-devel boost-devel boost-python3-devel boost-numpy3 flex tbb-devel blosc-devel
 
