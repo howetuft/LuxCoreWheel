cc6895632288f2ffc9575564f7c93a7fa5fde52d
howetuft <34142192+howetuft@users.noreply.github.com>
2024-08-09 16:03:17 +0000

    Conan

---
+++ conanfile.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2024 Howetuft
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from conan import ConanFile
+
+class LuxCore(ConanFile):
+    requirements = [
+        "opencolorio/2.3.1",
+        "minizip-ng/4.0.3",
+        "libpng/1.6.42",
+        "ffmpeg/6.1",
+        "boost/1.84.0",
+        "openimageio/2.5.14.0",
+    ]
+
+    options = {
+        "boost/*:without_python": False
+    }
+
+    generators = "CMakeDeps", "CMakeToolchain"
