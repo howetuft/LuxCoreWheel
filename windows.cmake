9aa6196d729e115334b4ed4ba62dfd882b8da3b3
howetuft <howetuft@gmail.com>
2024-08-12 19:20:43 +0200

    Conan

--- windows.cmake
+++ windows.cmake
@@ -2,6 +2,18 @@
 
 # Conan defines OpenImageIO_FOUND, whereas LuxCore
 # expects OPENIMAGEIO_FOUND. We will lure LuxCore:
-set(OPENIMAGEIO_FOUND "TRUE")
-set(EMBREE_FOUND "TRUE")
+find_package(OpenImageIO REQUIRED)
+if (OpenImageIO_FOUND)
+  set(OPENIMAGEIO_FOUND "TRUE")
+  MESSAGE(VERBOSE "LuxCoreWheel - OpenImageIO_INCLUDE_PATH: ${OpenImageIO_INCLUDE_PATH}")
+  include_directories(${OpenImageIO_INCLUDE_PATH})
+endif()
+
+find_package(Embree REQUIRED)
+if (Embree_FOUND)
+  set(EMBREE_FOUND "TRUE")
+  MESSAGE(VERBOSE "LuxCoreWheel - Embree: ${Embree_INCLUDE_PATH}")
+  include_directories(${Embree_INCLUDE_PATH})
+endif()
+
 set(OIDN_FOUND "TRUE")
