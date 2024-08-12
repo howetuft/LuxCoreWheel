bb1ccc786d775ef5830e803abc960061fa9b6237
howetuft <howetuft@gmail.com>
2024-08-12 19:49:49 +0200

    Conan

---
+++ configs/Config_Windows-Wheel.cmake
@@ -0,0 +1,31 @@
+###########################################################################
+#
+# Configuration
+#
+###########################################################################
+
+# Specialization for wheel Windows
+
+MESSAGE(STATUS "Using Windows wheel settings")
+
+find_package(OpenImageIO REQUIRED)
+if (OpenImageIO_FOUND)
+  set(OPENIMAGEIO_FOUND "TRUE")
+  MESSAGE(VERBOSE "LuxCoreWheel - OpenImageIO_INCLUDE_PATH: ${OIIO_INCLUDE_DIR}")
+  include_directories(${OpenImageIO_INCLUDE_DIR})
+else()
+  MESSAGE(VERBOSE "LuxCoreWheel - OpenImageIO NOT FOUND")
+endif()
+
+find_package(Embree REQUIRED)
+if (EMBREE_FOUND)
+  set(EMBREE_FOUND "TRUE")
+  MESSAGE(VERBOSE "LuxCoreWheel - Embree: ${EMBREE_INCLUDE_DIR}")
+  include_directories(${EMBREE_INCLUDE_DIR})
+else()
+  MESSAGE(VERBOSE "LuxCoreWheel - Embree NOT FOUND")
+endif()
+
+set(OIDN_FOUND "TRUE")
+
+SET(CMAKE_BUILD_TYPE "Release")
