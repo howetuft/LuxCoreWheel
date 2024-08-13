5d1a8ea0179e6c64e3269b281aa396897dede09c
howetuft <howetuft@gmail.com>
2024-08-13 15:01:43 +0200

    Conan

--- externals/windows/openimageio/conanfile.py
+++ externals/windows/openimageio/conanfile.py
@@ -295,6 +295,7 @@ class OpenImageIOConan(ConanFile):
             open_image_io.requires.append("ptex::ptex")
         if self.options.with_libwebp:
             open_image_io.requires.append("libwebp::libwebp")
+            self.cpp_info.components["libwebp::libwebp"].set_property("cmake_target_aliases", ["WebP::WebP"])
         if self.settings.os in ["Linux", "FreeBSD"]:
             open_image_io.system_libs.extend(["dl", "m", "pthread"])
 
