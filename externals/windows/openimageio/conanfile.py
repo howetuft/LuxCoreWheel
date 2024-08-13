2f0eefc26e97f239ac48b3e53d23c7f9dcbf0d7c
howetuft <howetuft@gmail.com>
2024-08-13 18:27:49 +0200

    Conan

--- externals/windows/openimageio/conanfile.py
+++ externals/windows/openimageio/conanfile.py
@@ -70,7 +70,7 @@ class OpenImageIOConan(ConanFile):
     def requirements(self):
         # Required libraries
         self.requires("boost/1.78.0")  # Modified
-        self.requires("openexr/2.5.7")  # Modified
+        self.requires("openexr/2.5.7", transitive_headers=True)  # Modified
         self.requires("zlib/[>=1.2.11 <2]")
         self.requires("libtiff/4.3.0")
         self.requires("imath/3.1.9", transitive_headers=True)
