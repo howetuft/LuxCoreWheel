5d96c2fd995f73c99797717adb37e808c3732a72
howetuft <howetuft@gmail.com>
2024-08-13 06:39:21 +0200

    Conan

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class LuxCore(ConanFile):
 
     requires = [
         "onetbb/2021.12.0",
-        "opencolorio/2.3.1",
+        "opencolorio/2.1.0",
         "minizip-ng/4.0.3",
         "libpng/1.6.42",
         "ffmpeg/6.1",
