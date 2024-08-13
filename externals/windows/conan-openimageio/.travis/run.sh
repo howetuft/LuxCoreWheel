86c6ab963841198ac2939a7caf2d77f629fdd373
howetuft <howetuft@gmail.com>
2024-08-13 06:08:17 +0200

    Conan

--- externals/windows/conan-openimageio/.travis/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-export CONAN_UPLOAD="https://api.bintray.com/conan/ppodsiadly/conan"
-python3 build.py
--- externals/windows/conan-openimageio/test_package/test_package.cpp
+++
@@ -1,9 +0,0 @@
-#include <OpenImageIO/imagecache.h>
-
-int main()
-{
-    using namespace OpenImageIO_v2_1;
-
-    ImageCache* cache = ImageCache::create();
-    ImageCache::destroy(cache);
-}
