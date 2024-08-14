9f7de26737f91422fa790471ec244f51f63884a8
howetuft <howetuft@gmail.com>
2024-08-14 05:18:57 +0200

    Conan

--- configs/Config_Windows-Wheel.cmake
+++ configs/Config_Windows-Wheel.cmake
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 Howetuft
+#
+# SPDX-License-Identifier: Apache-2.0
+
 ###########################################################################
 #
 # Configuration
@@ -11,19 +15,28 @@ MESSAGE(STATUS "Using Windows wheel settings")
 find_package(OpenImageIO REQUIRED)
 if (OpenImageIO_FOUND)
   set(OPENIMAGEIO_FOUND "TRUE")
-  MESSAGE(VERBOSE "LuxCoreWheel - OpenImageIO_INCLUDE_DIR: ${OpenImageIO_INCLUDE_DIR}")
+  MESSAGE(STATUS "LuxCoreWheel - OpenImageIO_INCLUDE_DIR: ${OpenImageIO_INCLUDE_DIR}")
   include_directories(${OpenImageIO_INCLUDE_DIR})
 else()
-  MESSAGE(VERBOSE "LuxCoreWheel - OpenImageIO NOT FOUND")
+  MESSAGE(STATUS "LuxCoreWheel - OpenImageIO NOT FOUND")
 endif()
 
 find_package(embree REQUIRED)
 if (embree_FOUND)
   set(EMBREE_FOUND "TRUE")
-  MESSAGE(VERBOSE "LuxCoreWheel - Embree: ${embree_INCLUDE_DIR}")
+  MESSAGE(STATUS "LuxCoreWheel - Embree: ${embree_INCLUDE_DIR}")
   include_directories(${embree_INCLUDE_DIR})
 else()
-  MESSAGE(VERBOSE "LuxCoreWheel - Embree NOT FOUND")
+  MESSAGE(STATUS "LuxCoreWheel - Embree NOT FOUND")
+endif()
+
+find_package(onetbb REQUIRED)
+if (onetbb_FOUND)
+  set(TBB_FOUND "TRUE")
+  MESSAGE(STATUS "LuxCoreWheel - Onetbb: ${onetbb_INCLUDE_DIR}")
+  include_directories(${onetbb_INCLUDE_DIR})
+else()
+  MESSAGE(STATUS "LuxCoreWheel - Onetbb NOT FOUND")
 endif()
 
 set(OIDN_FOUND "TRUE")
