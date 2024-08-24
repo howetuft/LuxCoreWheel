922ba61652937b8d7c2e94ae08657e075d387901
howetuft <howetuft@gmail.com>
2024-08-24 09:00:19 +0200

    Conan

--- externals/windows/openimageio/conanfile.py
+++ externals/windows/openimageio/conanfile.py
@@ -140,11 +140,13 @@ class OpenImageIOConan(ConanFile):
     def layout(self):
         cmake_layout(self)
         self.cpp.package.libs = ["OpenImageIO", "OpenImageIO_Util"]
+        self.cpp.package.libdirs.append("lib")
         self.cpp.build.libdirs.append("lib")
+        self.cpp.build.includedirs.append("include")
         print("oiio layout:")
         print(f"build: {self.folders.build}")
         print(f"build.lib: {self.cpp.build.libdirs}")
-        print(f"build.lib: {self.cpp.build.includedirs}")
+        print(f"build.include: {self.cpp.build.includedirs}")
         print(f"source.include: {self.cpp.source.includedirs}")
         return
 
