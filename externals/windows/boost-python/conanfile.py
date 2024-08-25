aece4e87b8b927e365921cde50bb4cd63c26b994
howetuft <howetuft@gmail.com>
2024-08-25 08:59:38 +0200

    Conan

--- externals/windows/boost-python/conanfile.py
+++ externals/windows/boost-python/conanfile.py
@@ -1,70 +1,13 @@
-from .. import boost_template
+# SPDX-FileCopyrightText: 2024 Howetuft
+#
+# SPDX-License-Identifier: Apache-2.0
 
-Boost_Python_Recipe = BoostModule("python", "1.78.0", ["config"])
+import os
+import sys
+here = os.path.dirname(__file__)
+sys.path.append(os.path.join(here, '..'))
 
+from boost_helper import BoostMeta, ConanFile
 
-# from conan import ConanFile
-# from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
-# from conan.tools.files import get
-
-
-# class boost_pythonRecipe(ConanFile):
-    # name = "boost-python"
-    # version = "1.78.0"
-    # package_type = "library"
-
-    # # Optional metadata
-    # license = "<Put the package license here>"
-    # author = "<Put your name here> <And your email here>"
-    # url = "<Package recipe repository url here, for issues about the package>"
-    # description = "<Description of boost-python package here>"
-    # topics = ("<Put some tag here>", "<here>", "<and here>")
-
-    # # Binary configuration
-    # settings = "os", "compiler", "build_type", "arch"
-    # options = {"shared": [True, False], "fPIC": [True, False]}
-    # default_options = {"shared": False, "fPIC": True}
-
-    # # Sources are located in the same place as this recipe, copy them to the recipe
-    # exports_sources = "CMakeLists.txt", "src/*", "include/*"
-
-    # # TODO hardcoded version
-    # requires = "boost/1.78.0"
-
-    # def source(self):
-        # # TODO hardcoded version
-        # get(
-            # self,
-            # "https://github.com/boostorg/python/archive/refs/tags/boost-1.78.0.zip",
-            # strip_root=True
-        # )
-
-    # def config_options(self):
-        # if self.settings.os == "Windows":
-            # self.options.rm_safe("fPIC")
-
-    # def configure(self):
-        # if self.options.shared:
-            # self.options.rm_safe("fPIC")
-
-    # def layout(self):
-        # cmake_layout(self)
-
-    # def generate(self):
-        # deps = CMakeDeps(self)
-        # deps.generate()
-        # tc = CMakeToolchain(self)
-        # tc.generate()
-
-    # def build(self):
-        # cmake = CMake(self)
-        # cmake.configure()
-        # cmake.build()
-
-    # def package(self):
-        # cmake = CMake(self)
-        # cmake.install()
-
-    # def package_info(self):
-        # self.cpp_info.libs = ["boost-python"]
-
+class Boost_Python(ConanFile, metaclass=BoostMeta, module="python", boost_deps=["config"]):
+    pass
