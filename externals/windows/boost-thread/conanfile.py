23031538e5bfe349a5acadd2570b31698430e642
howetuft <howetuft@gmail.com>
2024-08-27 22:01:17 +0200

    Conan

---
+++ externals/windows/boost-thread/conanfile.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2024 Howetuft
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from boost_helper import BoostMeta, ConanFile
+
+DEPS = [
+    "assert",
+    "bind",
+    "concept_check",
+    "config",
+    "container_hash",
+    "core",
+    "function",
+    "intrusive",
+    "io",
+    "iterator",
+    "move",
+    "optional",
+    "predef",
+    "preprocessor",
+    "smart_ptr",
+    "static_assert",
+    "throw_exception",
+    "tuple",
+    "type_traits",
+    "utility",
+    "winapi",
+    "algorithm",
+    "lexical_cast",
+]
+
+class BoostThread(ConanFile, metaclass=BoostMeta, module="thread", boost_deps=DEPS):
+    pass
