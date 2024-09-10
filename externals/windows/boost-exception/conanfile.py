4384dd15611c6541c867a7ecacf5b3bf41b2191f
howetuft <howetuft@gmail.com>
2024-09-10 08:41:22 +0200

    Conan

--- externals/windows/boost-exception/conanfile.py
+++ externals/windows/boost-exception/conanfile.py
@@ -6,6 +6,12 @@ from boost_helper import BoostMeta, ConanFile
 
 DEPS = [
     "assert",
+    "config",
+    "core",
+    "smart_ptr",
+    "throw_exception",
+    "tuple",
+    "type_traits",
 ]
 
 class BoostException(ConanFile, metaclass=BoostMeta, module="exception", boost_deps=DEPS, ):
