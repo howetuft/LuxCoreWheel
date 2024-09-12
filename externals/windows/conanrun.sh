b58c6f516a274fd051d8089b94a7fef5ffcba8fa
howetuft <howetuft@gmail.com>
2024-09-12 13:37:13 +0200

    Conan

---
+++ externals/windows/conanrun.sh
@@ -0,0 +1,1 @@
+. "/home/vincent/.masse/Documents/DevGit/LuxCoreWheel/externals/windows/conanrunenv-release-x86_64.sh"
---
+++ externals/windows/deactivate_conanbuild.sh
@@ -0,0 +1,1 @@
+. "/home/vincent/.masse/Documents/DevGit/LuxCoreWheel/externals/windows/deactivate_conanbuildenv-release-x86_64.sh"
