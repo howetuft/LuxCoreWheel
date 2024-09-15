865ed37ebe1597b697ff2f6b544a71a216032b04
howetuft <howetuft@gmail.com>
2024-09-15 15:27:33 +0200

    Conan

--- externals/windows/boost-boost/conanfile.py
+++ externals/windows/boost-boost/conanfile.py
@@ -9,9 +9,10 @@ from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 from conan.tools.files import *
 
-from boost_helper import DEPENDENCIES
+from boost_helper import DEPENDENCIES, LIBRARIES
 
 DEPS = DEPENDENCIES.keys()
+
 
 class BoostBoost(ConanFile):
     name = "boost"
@@ -26,6 +27,8 @@ class BoostBoost(ConanFile):
     package_type = "header-library"
 
     def requirements(self):
+        if self.settings.os != "Windows":
+            del DEPENDENCIES["python"]  # Debug
         for dep in DEPS:
             self.requires(
                 f"boost-{dep}/{self.version}@luxcorewheels/luxcorewheels",
@@ -34,6 +37,40 @@ class BoostBoost(ConanFile):
             )
             print(f"Boost package: requires {dep}")
 
+    def source(self):
+        # We generate CMakeLists
+        header = [
+            'message(STATUS "Running cmake on Boost")\n',
+            "cmake_minimum_required(VERSION 3.5...3.16)\n",
+            "project(Boost VERSION 1.78.0 LANGUAGES CXX)\n",
+            "add_library(boost INTERFACE)\n",
+            "add_library(Boost ALIAS boost)\n",
+        ]
+        finds = [f"find_package(Boost_{dep})\n" for dep in DEPENDENCIES]
+        links = (
+            ["target_link_libraries(boost INTERFACE \n"]
+            + [f"Boost::{dep}\n" for dep in DEPENDENCIES]
+            + [")\n"]
+        )
+        includes = (
+            ["target_include_directories(boost INTERFACE \n"]
+            + [f"Boost::{dep}\n" for dep in DEPENDENCIES]
+            + [")\n"]
+        )
+
+        content = header + finds + links + includes
+
+        filepath = os.path.join(self.source_folder, "CMakeLists.txt")
+        with open(filepath, "w", encoding="utf-8") as f:
+            f.writelines(content)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self)
@@ -48,7 +85,7 @@ class BoostBoost(ConanFile):
         self.cpp.package.includedirs = ["include"]
         self.cpp.package.libdirs += [
             self.folders.build,
-            os.path.join(self.folders.build, "lib")
+            os.path.join(self.folders.build, "lib"),
         ]
 
         # Describe what changes between package and editable
@@ -77,21 +114,44 @@ class BoostBoost(ConanFile):
     def package(self):
         cmake = CMake(self)
         cmake.install()
-        copy(self, "*.h", src=self.source_folder,
-             dst=os.path.join(self.package_folder, "include"))
-        copy(self, "*.hpp", src=self.source_folder,
-             dst=os.path.join(self.package_folder, "include"))
-        copy(self, "*.lib", src=self.build_folder,
-             dst=os.path.join(self.package_folder, "lib"), keep_path=False)
-        copy(self, "*.a", src=self.build_folder,
-             dst=os.path.join(self.package_folder, "lib"), keep_path=False)
-        copy(self, "*.cmake", src=self.build_folder,
-             dst=os.path.join(self.package_folder, "lib", "cmake"), keep_path=False)
+        copy(
+            self,
+            "*.h",
+            src=self.source_folder,
+            dst=os.path.join(self.package_folder, "include"),
+        )
+        copy(
+            self,
+            "*.hpp",
+            src=self.source_folder,
+            dst=os.path.join(self.package_folder, "include"),
+        )
+        copy(
+            self,
+            "*.lib",
+            src=self.build_folder,
+            dst=os.path.join(self.package_folder, "lib"),
+            keep_path=False,
+        )
+        copy(
+            self,
+            "*.a",
+            src=self.build_folder,
+            dst=os.path.join(self.package_folder, "lib"),
+            keep_path=False,
+        )
+        copy(
+            self,
+            "*.cmake",
+            src=self.build_folder,
+            dst=os.path.join(self.package_folder, "lib", "cmake"),
+            keep_path=False,
+        )
 
     def package_info(self):
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
-        #self.cpp_info.libs = [f"boost_{self.module}"]
+        # self.cpp_info.libs = [f"boost_{self.module}"]
         self.cpp_info.set_property("cmake_file_name", f"Boost")
         self.cpp_info.set_property("cmake_target_name", f"boost::boost")
         self.cpp_info.set_property("cmake_target_aliases", ["boost", "Boost"])
@@ -100,13 +160,11 @@ class BoostBoost(ConanFile):
 
         # Add components
         # for dep in DEPENDENCIES:
-            # component = self.cpp_info.components[dep]
-            # component.set_property("cmake_target_name", f"boost::{dep}")
-            # component.set_property("cmake_target_aliases", [f"Boost::{dep}"])
-            # component.requires = DEPENDENCIES[dep]
+        # component = self.cpp_info.components[dep]
+        # component.set_property("cmake_target_name", f"boost::{dep}")
+        # component.set_property("cmake_target_aliases", [f"Boost::{dep}"])
+        # component.requires = DEPENDENCIES[dep]
 
     def package_id(self):
         # We clear everything in order to have a constant package_id and use the cache
         self.info.clear()
-
-
