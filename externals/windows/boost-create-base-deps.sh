b1095570d72fde875aa8793fe043481af0526ac3
howetuft <howetuft@gmail.com>
2024-09-19 20:40:22 +0200

    Conan

--- externals/windows/boost-create-base-deps.sh
+++ externals/windows/boost-create-base-deps.sh
@@ -112,7 +112,7 @@ conan_create_recipe() {
 conan_build_recipe() {
   local destdir=~/.boost_conan/${1}
 
-  conan install "${destdir}" -s build_type=Release
+  conan install "${destdir}" -s build_type=Release --build=missing
 
   conan source "${destdir}"
   #conan editable add $destdir
