e0c0eb9b02626563a883706cfbeb5355818efe63
howetuft <34142192+howetuft@users.noreply.github.com>
2024-09-19 15:07:48 +0000

    Conan

--- externals/windows/boost-create-base-deps.sh
+++ externals/windows/boost-create-base-deps.sh
@@ -98,10 +98,11 @@ conan_create_recipe() {
 
   # Create recipe
   local destdir=~/.boost_conan/${1}
+  local origdir=$GITHUB_WORKSPACE/external/windows
   echo "Creating ${destdir}"
   mkdir -p ${destdir}
 
-  sed "s/MODULE/$1/" boost-base-dep-template.txt > ${destdir}/conanfile.py
+  sed "s/MODULE/$1/" ${origdir}/boost-base-dep-template.txt > ${destdir}/conanfile.py
 
   conan install "${destdir}" -s build_type=Release
 
@@ -156,7 +157,7 @@ echo "LuxCoreWheels - BUILDING BOOST"
 
 # Create boost package
 boost_destdir=~/.boost_conan/boost
-cp -R boost-boost ${boost_destdir}
+cp -R ${GITHUB_WORKSPACE}/external/windows/boost-boost ${boost_destdir}
 conan source ${boost_destdir}  # Create CMakeLists
 conan editable add ${boost_destdir}
 #conan install ${boost_destdir} --build=editable -s build_type=Release
