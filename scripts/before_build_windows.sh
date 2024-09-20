992c784cb54da00555e52431bb17af31ec76ea2b
howetuft <34142192+howetuft@users.noreply.github.com>
2024-09-20 12:44:06 +0000

    Conan

--- scripts/before_build_windows.sh
+++ scripts/before_build_windows.sh
@@ -1,19 +1,17 @@
-cd $GITHUB_WORKSPACE
-
 echo "CIBW_BEFORE_BUILD: Boost"
-source externals/windows/boost-create-base-deps.sh
+source $GITHUB_WORKSPACE/externals/windows/boost-create-base-deps.sh
 
 echo "CIBW_BEFORE_BUILD: OIIO"
-oiio=./externals/windows/openimageio
+oiio=$GITHUB_WORKSPACE/externals/windows/openimageio
 conan editable add ${oiio}
 conan install ${oiio} -s build_type=Release
 conan source ${oiio}
 
 echo "CIBW_BEFORE_BUILD: OIDN"
-oidn=./externals/windows/oidn
+oidn=$GITHUB_WORKSPACE/externals/windows/oidn
 conan editable add ${oidn}
 conan install ${oidn} -s build_type=Release
 conan source ${oidn}
 
 echo "CIBW_BEFORE_BUILD: LuxCore"
-conan install . -s build_type=Release --build=missing
+conan install $GITHUB_WORKSPACE -s build_type=Release --build=missing
