820bb3cd4dcbfcde44f2bd10ca744a82bffb3e93
howetuft <howetuft@gmail.com>
2024-09-21 12:05:00 +0200

    Conan

--- scripts/before_build_windows.sh
+++ scripts/before_build_windows.sh
@@ -14,5 +14,5 @@ conan install ${oidn} -s build_type=Release
 conan source ${oidn}
 
 echo "CIBW_BEFORE_BUILD: LuxCore"
-conan editable add $GITHUB_WORKSPACE --name=LuxCoreWheel --version=2.6.0
+conan editable add $GITHUB_WORKSPACE --name=LuxCoreWheels --version=2.6.0
 conan install $GITHUB_WORKSPACE -s build_type=Release --build=missing --build=editable
