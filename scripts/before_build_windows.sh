fe5ec12f3c4c234177be91f4a3e66b8397eb181f
howetuft <howetuft@gmail.com>
2024-09-22 22:26:19 +0200

    Conan

--- scripts/before_build_windows.sh
+++ scripts/before_build_windows.sh
@@ -3,10 +3,10 @@ source $GITHUB_WORKSPACE/externals/windows/boost-create-base-deps.sh
 
 echo "CIBW_BEFORE_BUILD: OIIO"
 oiio=$GITHUB_WORKSPACE/externals/windows/openimageio
-conan install ${oiio} --build=editable -s build_type=Release openimageio/2.2.13.1@LuxCoreWheels/LuxCoreWheels
+conan install ${oiio} --build=editable -s build_type=Release
 conan source ${oiio}
 conan build ${oiio}
-conan editable add ${oiio} openimageio/2.2.13.1@LuxCoreWheels/LuxCoreWheels
+conan editable add ${oiio}
 
 #conan install ${oiio} --profile=conan_profile -s build_type=Release
 #conan build ${oiio} --build=editable -s build_type=Release
