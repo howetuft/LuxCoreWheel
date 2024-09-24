dc794d726135d237febb3a22c83ef2eb48a539f1
howetuft <34142192+howetuft@users.noreply.github.com>
2024-09-24 17:44:45 +0000

    Conan

--- scripts/before_build_windows.sh
+++ scripts/before_build_windows.sh
@@ -3,6 +3,7 @@ source $GITHUB_WORKSPACE/externals/windows/boost-create-base-deps.sh
 
 # https://github.com/conan-io/conan/issues/13400
 echo "CIBW_BEFORE_BUILD: OIIO"
+conan install --requires=openexr/2.5.7 --build=openexr/2.5.7 -s build_type=Release
 oiio=$GITHUB_WORKSPACE/externals/windows/openimageio
 #conan install ${oiio} --build=editable -s build_type=Release
 conan editable add ${oiio}
@@ -20,11 +21,8 @@ conan source ${oidn}
 #conan build ${oidn} --build=editable -s build_type=Release
 
 echo "CIBW_BEFORE_BUILD: LuxCore"
-conan install --requires=openexr/2.5.7 --build=openexr/2.5.7 -s build_type=Release
 conan editable add $GITHUB_WORKSPACE --name=LuxCoreWheels --version=2.6.0 --user=LuxCoreWheels --channel=LuxCoreWheels
-conan install --requires=LuxCoreWheels/2.6.0@LuxCoreWheels/LuxCoreWheels \
-    --profile=conan_profile --build=editable \
-    -s build_type=Release
+conan install --requires=LuxCoreWheels/2.6.0@LuxCoreWheels/LuxCoreWheels --profile=conan_profile --build=editable -s build_type=Release
 #conan install $GITHUB_WORKSPACE --profile=conan_profile -s build_type=Release
 #conan source $GITHUB_WORKSPACE
 #conan build $GITHUB_WORKSPACE --build=editable -s build_type=Release
