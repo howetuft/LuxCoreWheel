3ecc8ac233319803b3542141b2297c9c4693570b
howetuft <howetuft@gmail.com>
2024-10-09 22:46:13 +0200

    Macos-13

--- scripts/before_build_macos.sh
+++ scripts/before_build_macos.sh
@@ -28,7 +28,7 @@ wait
 
 # TODO In oidn conanfile?
 mkdir $GITHUB_WORKSPACE/libs
-cp -rv $GITHUB_WORKSPACE/externals/windows/oidn_macos/oidn-2.3.0.x86_64.macos/bin/. $GITHUB_WORKSPACE/libs/
+cp -rv $GITHUB_WORKSPACE/externals/windows/oidn_macos13/oidn-2.3.0.x86_64.macos/bin/. $GITHUB_WORKSPACE/libs/
 
 echo "CIBW_BEFORE_BUILD: LuxCore"
 conan editable add $GITHUB_WORKSPACE --name=LuxCoreWheels --version=2.6.0 --user=LuxCoreWheels --channel=LuxCoreWheels
