8b0baf773eaa9d3be0adca7b36238376129c30dd
howetuft <howetuft@gmail.com>
2024-11-01 07:27:56 +0100

    Python3.13

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ class LuxCore(ConanFile):
 
     requires = [
         "onetbb/2020.3",
-        "opencolorio/2.1.0",
+        "opencolorio/2.4.0",
         "minizip-ng/4.0.3",
         "libpng/1.6.42",
         "spdlog/1.8.5",
