277c7de63343832e69f73d651b47ac883ad1eda1
howetuft <howetuft@gmail.com>
2024-11-01 09:09:53 +0100

    Python3.13

--- conanfile.py
+++ conanfile.py
@@ -30,7 +30,7 @@ class LuxCore(ConanFile):
     ]
 
     default_options = {
-        "fmt/*:header_only": True,
+        # "fmt/*:header_only": True,
         "spdlog/*:header_only": True,
         "embree3/*:neon": True,
     }
