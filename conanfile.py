68bffbb33db557af6bef507eb8f85a310cea0716
howetuft <howetuft@gmail.com>
2024-11-01 07:32:14 +0100

    Python3.13

--- conanfile.py
+++ conanfile.py
@@ -41,6 +41,7 @@ class LuxCore(ConanFile):
     def requirements(self):
         self.requires(f"boost/{_boost_version}", override=True)
         self.requires(f"boost-python/{_boost_version}@luxcorewheels/luxcorewheels")
+        self.requires(f"fmt/10.2.1", override=True)
         if self.settings.os == "Linux":
             self.requires("oidn/1.2.4@luxcorewheels/luxcorewheels")
         else:
