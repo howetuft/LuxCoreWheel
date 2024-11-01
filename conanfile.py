fba448f4110455b4b753994e6d642b4fb295fc6b
howetuft <howetuft@gmail.com>
2024-11-01 07:12:58 +0100

    Python3.13

--- conanfile.py
+++ conanfile.py
@@ -24,7 +24,7 @@ class LuxCore(ConanFile):
         "minizip-ng/4.0.3",
         "libpng/1.6.42",
         "spdlog/1.8.5",
-        f"openimageio/{_oiio_version}@luxcorewheels/luxcorewheels",
+        f"openimageio/{_oiio_version}",
         "c-blosc/1.21.5",
         "openexr/2.5.7",
         f"boost/{_boost_version}",
