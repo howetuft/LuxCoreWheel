de3e952d83d0eb39edf15e0c5372abce4d07cf09
howetuft <howetuft@gmail.com>
2024-11-02 13:08:41 +0100

    Python3.13

--- scripts/install_conan.sh
+++ scripts/install_conan.sh
@@ -39,7 +39,7 @@ if [[ $RUNNER_OS == "macOS" && $RUNNER_ARCH == "ARM64" ]]; then
 fi
 
 echo "CIBW_BEFORE_BUILD: OCIO"
-conan source opencolorio/2.3.1
+conan download opencolorio/2.3.1 --only-recipe
 conan editable add $(conan cache path opencolorio/2.3.1)
 
 if [[ $RUNNER_OS == "macOS" ]]; then
