e5f7acbef241243b9ecf69f11d691367b88dfd20
howetuft <howetuft@gmail.com>
2024-11-02 12:39:16 +0100

    Python3.13

--- scripts/install_conan.sh
+++ scripts/install_conan.sh
@@ -49,7 +49,7 @@ wait
 
 echo "CIBW_BEFORE_BUILD: LuxCore"
 conan editable add $WORKSPACE --name=LuxCoreWheels --version=2.6.0 --user=LuxCoreWheels --channel=LuxCoreWheels
-conan install \
+conan graph explain \
   --requires=LuxCoreWheels/2.6.0@LuxCoreWheels/LuxCoreWheels \
   --profile:all=$WORKSPACE/conan_profiles/conan_profile_${RUNNER_OS}_${RUNNER_ARCH} \
   --build=editable \
