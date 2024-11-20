c74c8e7d7879ef8a8a207f0411462aa40317a2da
howetuft <howetuft@gmail.com>
2024-11-20 23:25:38 +0100

    cache

--- scripts/install_conan.sh
+++ scripts/install_conan.sh
@@ -3,8 +3,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 set -o pipefail
+if [[ $RUNNER_OS == "Linux" ]]; then
+  cache_dir=/conan_cache
+else
+  cache_dir=$WORKSPACE/conan_cache
+fi
 
-echo "CIBW_BEFORE_BUILD: pip"
+echo "::group::CIBW_BEFORE_BUILD: pip"
 pip install conan
 pip install ninja
 if [[ $PYTHON_MINOR == "8" ]]; then
@@ -12,24 +17,33 @@ if [[ $PYTHON_MINOR == "8" ]]; then
 else
   pip install "numpy >= 2" &
 fi
+echo "::endgroup::"
+
+echo "::group::CIBW_BEFORE_BUILD: conan cache"
+ls $cache_dir
+conan cache restore -vverbose $cache_dir/conan_cache_save.tgz
+echo "::endgroup::"
 
 conan_path=$WORKSPACE/deps/conan
 
-echo "CIBW_BEFORE_BUILD: Boost Python"
+echo "::group::CIBW_BEFORE_BUILD: Boost Python"
 boost_python=$conan_path/boost-python
 conan editable add ${boost_python}
 conan source ${boost_python} &
+echo "::endgroup::"
 
-echo "CIBW_BEFORE_BUILD: OIIO"
+echo "::group::CIBW_BEFORE_BUILD: OIIO"
 unset CI  # Otherwise OIIO passes -Werror to compiler (MacOS)!
 oiio=$conan_path/openimageio
 conan editable add ${oiio}
 conan source ${oiio} &
+echo "::endgroup::"
 
-echo "CIBW_BEFORE_BUILD: OIDN"
+echo "::group::CIBW_BEFORE_BUILD: OIDN"
 oidn=$conan_path/oidn_${RUNNER_OS}_${RUNNER_ARCH}
 conan editable add ${oidn}
 conan source ${oidn} &
+echo "::endgroup::"
 
 
 if [[ $RUNNER_OS == "macOS" && $RUNNER_ARCH == "ARM64" ]]; then
@@ -41,7 +55,7 @@ fi
 
 wait
 
-echo "CIBW_BEFORE_BUILD: LuxCore"
+echo "::group::CIBW_BEFORE_BUILD: LuxCore"
 conan editable add $WORKSPACE --name=LuxCoreWheels --version=2.6.0 --user=LuxCoreWheels --channel=LuxCoreWheels
 conan install \
   --requires=LuxCoreWheels/2.6.0@LuxCoreWheels/LuxCoreWheels \
@@ -51,8 +65,9 @@ conan install \
   --deployer=runtime_deploy \
   --deployer-folder=$WORKSPACE/libs \
   -s build_type=Release
+echo "::endgroup::"
 
-echo "Installing oidn"
+echo "::group::Installing oidn"
 if [[ $RUNNER_OS == "Linux" ]]; then
     oidn_version=2.3.0
     cp -rv $oidn/oidn-${oidn_version}.x86_64.linux/bin/. $WORKSPACE/libs/
@@ -73,3 +88,9 @@ else
       echo "ERROR: unhandled runner os/arch '${RUNNER_OS}/${RUNNER_ARCH}'"
       exit 64
 fi
+echo "::endgroup::"
+
+echo "::group::Saving dependencies in ${cache_dir}"
+conan cache save -vverbose --file $cache_dir/conan_cache_save.tgz "*/*:*"
+ls $cache_dir  # Check
+echo "::endgroup::"
