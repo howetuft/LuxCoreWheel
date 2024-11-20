70854c6b0c24c7088610d66073efac1bd3e429a2
howetuft <howetuft@gmail.com>
2024-11-20 21:00:32 +0100

    Address sanitizer

--- conan_profiles/myvars.cmake
+++ conan_profiles/myvars.cmake
@@ -1,1 +1,4 @@
-set(EMBREE_STACK_PROTECTOR ON)
+option(EMBREE_STACK_PROTECTOR "Embree stack protector" ON)
+if (${CMAKE_PROJECT_NAME} EQUAL "embree3")
+  add_compile_options(-g -fno-common)
+endif()
